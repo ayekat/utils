bdf0c1eedc5c5c592bf6b3e53d57f86729bc3fa7
ayekat <takeya@bluewin.ch>
2015-12-16 13:43:02 +0100

    Add base for X event and signal handling, restructure
    The signal handling is not implemented yet, and the ButtonPress X event
    on the root window causes a BadAccess error, because only one client at
    the time can grab ButtonPress events, and currently karuiwm already does
    that (http://www.cplusplusdevelop.com/775_14578155/, there is no need
    for it though, will get that one fixed).

--- xgol.c
+++ xgol.c
@@ -23,7 +23,7 @@ struct {
 	Pixmap pm;
 	int depth;
 	GC gc;
-
+	int xfd;
 } xgol;
 
 struct {
@@ -38,10 +38,14 @@ struct {
 static size_t count_neighbours(int unsigned cx, int unsigned cy);
 static void step_generation(void);
 static void draw_world(void);
+static void init_signals(void);
 static void init_x(void);
 static void init_world(void);
-static void term_x(void);
+static void xevent(void);
+static void block(struct timeval interrupt);
+static void run(void);
 static void term_world(void);
+static void term_x(void);
 
 static size_t
 count_neighbours(int unsigned cx, int unsigned cy)
@@ -49,11 +53,14 @@ count_neighbours(int unsigned cx, int unsigned cy)
 	size_t neighbours = 0;
 	int x, y;
 
-	for (x = (int signed) cx - 1; x <= (int signed) cx + 1; ++x)
-		for (y = (int signed) cy - 1; y <= (int signed) cy + 1; ++y)
-			if ((x != cx || y != cy)
-			&& x >= 0 && x < world.w && y >= 0 && y < world.h)
+	for (x = (int signed) cx - 1; x <= (int signed) cx + 1; ++x) {
+		for (y = (int signed) cy - 1; y <= (int signed) cy + 1; ++y) {
+			if ((x != (int signed) cx || y != (int signed) cy)
+			&& x >= 0 && x < (int signed) world.w
+			&& y >= 0 && y < (int signed) world.h)
 				neighbours += world.cur[x][y];
+		}
+	}
 	return neighbours;
 }
 
@@ -103,24 +110,37 @@ draw_world(void)
 }
 
 static void
+init_signals(void)
+{
+	/* TODO */
+}
+
+static void
 init_x(void)
 {
 	XGCValues xgcv;
 
+	/* basic X */
 	xgol.dpy = XOpenDisplay(NULL);
 	if (xgol.dpy == NULL)
 		FATAL("could not open X");
+	xgol.xfd = ConnectionNumber(xgol.dpy);
 	xgol.screen = DefaultScreen(xgol.dpy);
-	monitor.w = (int unsigned) DisplayWidth(xgol.dpy, xgol.screen);
-	monitor.h = (int unsigned) DisplayHeight(xgol.dpy, xgol.screen);
-	world.w = monitor.w / SIZE;
-	world.h = monitor.h / SIZE;
 	xgol.root = RootWindow(xgol.dpy, xgol.screen);
 	xgol.depth = DefaultDepth(xgol.dpy, xgol.screen);
+
+	/* pixmap */
+	monitor.w = (int unsigned) DisplayWidth(xgol.dpy, xgol.screen);
+	monitor.h = (int unsigned) DisplayHeight(xgol.dpy, xgol.screen);
 	xgol.pm = XCreatePixmap(xgol.dpy, xgol.root, monitor.w, monitor.h,
 	                        (int unsigned) xgol.depth);
+
+	/* graphics context */
 	xgcv.graphics_exposures = False;
 	xgol.gc = XCreateGC(xgol.dpy, xgol.root, GCGraphicsExposures, &xgcv);
+
+	/* events */
+	//XSelectInput(xgol.dpy, xgol.root, ButtonPressMask);
 }
 
 static void
@@ -128,6 +148,10 @@ init_world(void)
 {
 	int unsigned x, y;
 
+	world.w = monitor.w / SIZE;
+	world.h = monitor.h / SIZE;
+
+	/* allocate */
 	world.cur = calloc(world.w, sizeof(bool *));
 	world.next = calloc(world.w, sizeof(bool *));
 	for (x = 0; x < world.w; ++x) {
@@ -135,11 +159,67 @@ init_world(void)
 		world.next[x] = calloc(world.h, sizeof(bool));
 	}
 
+	/* populate */
 	srand((unsigned int) time(NULL));
 	for (x = 0; x < world.w; ++x)
 		for (y = 0; y < world.h; ++y)
 			world.cur[x][y] = rand() % 2;
+}
 
+static void
+xevent(void)
+{
+	XEvent xev;
+
+	if (!XCheckMaskEvent(xgol.dpy, ExposureMask|ButtonPressMask, &xev)) {
+		fprintf(stderr, "unmaked X event\n");
+		return;
+	}
+
+	switch (xev.type) {
+	case Expose:
+		/* TODO */
+		printf("ExposeEvent!\n");
+		break;
+	case ButtonPressMask:
+		/* TODO */
+		printf("ButtonPressEvent!\n");
+		break;
+	default:
+		fprintf(stderr, "masked but unhandled X event: %d", xev.type);
+	}
+}
+
+static void
+block(struct timeval interval)
+{
+	fd_set fds;
+	int s;
+
+	do {
+		FD_ZERO(&fds);
+		FD_SET(xgol.xfd, &fds);
+		s = select(FD_SETSIZE, &fds, NULL, NULL, &interval);
+		if (s <= 0)
+			break;
+		if (FD_ISSET(xgol.xfd, &fds))
+			xevent();
+	} while (interval.tv_usec > 0);
+}
+
+static void
+run(void)
+{
+	struct timeval interval = {
+		.tv_sec = 0,
+		.tv_usec = 100000
+	};
+
+	while (true) {
+		step_generation();
+		draw_world();
+		block(interval);
+	}
 }
 
 static void
@@ -166,15 +246,10 @@ term_world(void)
 int
 main(void)
 {
+	init_signals();
 	init_x();
 	init_world();
-
-	while (true) {
-		step_generation();
-		draw_world();
-		usleep(100000);
-	}
-
+	run();
 	term_world();
 	term_x();
 
